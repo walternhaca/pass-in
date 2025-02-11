aaa798234b79fbc89347b7118111b15e0e7d1505
Walter Nhaca <nhacawalter@gmail.com>
2025-02-11 01:36:44 +0000

    Configurado driver do banco de dados com criacao de pasta database em resources e alteracao da estrutura da dependecia lombok

---
+++ src/test/java/com/example/pass_in/PassInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pass_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
